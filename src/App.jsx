27b708e04536b3ddea120e695d8aea3b9931c732
DrashtiS <drashtisanjayshah@gmail.com>
2024-01-13 13:13:07 +0530

    apply changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Drashti Shah",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "drashtisanjayshah@gmail.com",
+  gitHub: "DrashtiSanjayShah",
+  instagram: "thatonegirlinb.tech",
+  linkedIn: "drashtisanjayshah",
+  youTube: "@thatonegirlinbtech",
 };
 
 const primaryColor = "#4E567E";
@@ -42,7 +40,9 @@ const App = () => {
   return (
     <div id="main">
       <Header />
-      <Home name={siteProps.name} title={siteProps.title} />
+      <div className="name">
+        <Home name={siteProps.name} title={siteProps.title} />
+      </div>
       <About />
       <Portfolio />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Python Prowess",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created a beginner friendly Playlist of YouTube Videos to help explain the basics of Python and how to move forward.",
+    url: "https://youtube.com/playlist?list=PLI9IU_4HLCWGRkLqlP50xvdb1W-_qMJd5&si=iw1BGk5JYITUKKYa",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created using Firebase Storage and deployed to Firebase. Includes my experience and design abilities.",
+    url: "https://portfolio-c4508.web.app",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Snake Game using Python and Pygame",
+    description: "Using the Pygame library, I created the classic Snake Game.",
+    url: "https://6548e205c496e52947209a45--melodious-cucurucho-a6b941.netlify.app/",
+  },
+  {
+    title: "Secure Transactions using Blockchain",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Made a project using Solidity and Ganache to create a secure transaction system using Blockchain.",
+    url: "https://6548e205c496e52947209a45--melodious-cucurucho-a6b941.netlify.app/",
   },
 ];
 
